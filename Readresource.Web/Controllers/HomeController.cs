a221b79326013e40e28dc474d5dc29ca5c09fb9a
Balakrishnan, Madhu (Cognizant) <586154@cognizant.com>
2018-02-13 19:16:54 +0530

    Add project files.

---
+++ Readresource.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+using System.IO;
+using Newtonsoft.Json;
+using Readresource.Web.Models;
+
+namespace Readresource.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.SendMailStatus = string.Empty;
+            Assembly assem = Assembly.GetExecutingAssembly();
+            var path = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\DefaultTemplate.json";
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var jsonString = reader.ReadToEnd();
+                    var mailModel = JsonConvert.DeserializeObject<EmailModel>(jsonString);
+                    return View(mailModel);
+                }
+            }
+
+
+        }
+        [HttpPost]
+        public ActionResult Index(EmailModel mailModel)
+        {
+            ViewBag.SendMailStatus = "Mail sent";
+            return View(mailModel); 
+        }
+
+        
+    }
+}
---
+++ Readresource.Web/Models/EmailModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Readresource.Web.Models
+{
+
+    public class EmailModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        [MaxLength(200)]
+        public string Body { get; set; }
+    }
+
+   
+}
